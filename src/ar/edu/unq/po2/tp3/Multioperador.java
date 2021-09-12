6941b815d03c6bbb157e98126701cabf833942b7
Justeffie <florencia.aquinounq@gmail.com>
2021-09-12 19:24:44 -0300

    Remove codigo innecesario

--- src/ar/edu/unq/po2/tp3/Multioperador.java
+++ src/ar/edu/unq/po2/tp3/Multioperador.java
@@ -6,19 +6,19 @@ public class Multioperador {
 	
 	public long suma(ArrayList<Integer> numeros) {
 		return numeros.stream()
-				.reduce(0, (subTotal, numero) -> subTotal + numero, Integer::sum);
+				.reduce(0, (subTotal, numero) -> subTotal + numero);
 	}
 	
 	public long restar(ArrayList<Integer> numeros) {
 		int first = numeros.get(0);
 		numeros.remove(0);
 		return numeros.stream()
-				.reduce(first, (subTotal, numero) -> subTotal - numero, Integer::sum);
+				.reduce(first, (subTotal, numero) -> subTotal - numero);
 	}
 	
 	public long multiplicar(ArrayList<Integer> numeros) {
 		return numeros.stream()
-				.reduce(1, (subTotal, numero) -> subTotal * numero, Integer::sum);
+				.reduce(1, (subTotal, numero) -> subTotal * numero);
 	}
 
 }
