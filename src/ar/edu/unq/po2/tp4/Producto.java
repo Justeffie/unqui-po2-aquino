c0ac8b52abd1e974f9f72d8e30e6123ee685b425
Justeffie <florencia.aquinounq@gmail.com>
2021-09-15 18:47:17 -0300

    Resolución ejercicio 1 tp4

---
+++ src/ar/edu/unq/po2/tp4/Producto.java
@@ -0,0 +1,46 @@
+package ar.edu.unq.po2.tp4;
+
+public class Producto {
+	
+	private String nombre;
+	
+	private double precio;
+	
+	private boolean esPrecioCuidado = false;
+	
+	public Producto(String nombre, double precio) {
+		this.setNombre(nombre);
+		this.setPrecio(precio);
+	}
+	
+	public Producto(String nombre, double precio, boolean esPrecioCuidado) {
+		this.setNombre(nombre);
+		this.setPrecio(precio);
+		this.setEsPrecioCuidado(esPrecioCuidado);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public boolean esPrecioCuidado() {
+		return esPrecioCuidado;
+	}
+
+	public void setEsPrecioCuidado(boolean esPrecioCuidado) {
+		this.esPrecioCuidado = esPrecioCuidado;
+	}
+
+}
---
+++ src/ar/edu/unq/po2/tp4/Supermercado.java
@@ -0,0 +1,56 @@
+package ar.edu.unq.po2.tp4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Supermercado {
+	
+	private String nombre;
+	
+	private String direccion;
+	
+	public Supermercado(String nombre, String direccion) {
+		this.setNombre(nombre);
+		this.setDireccion(direccion);
+	}
+	
+	private List<Producto> productos = new ArrayList<Producto>();
+	
+	public long getCantidadDeProductos() {
+		return this.getProductos().size();
+	}
+
+	public double getPrecioTotal() {
+		return this.getProductos()
+				.stream()
+				.reduce(0.0, (subtotal, producto) -> subtotal + producto.getPrecio(), Double::sum);
+	}
+	
+	public void agregarProducto(Producto producto) {
+		this.productos.add(producto);
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+}
