413d4771b4f10bdf9c595d6ab319f7adbdcd75e5
Justeffie <florencia.aquinounq@gmail.com>
2021-09-15 19:09:22 -0300

    Resolución ejercicio 2

--- src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
+++ src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
@@ -1,12 +1,32 @@
 package ar.edu.unq.po2.tp4;
 
 public class ProductoPrimeraNecesidad extends Producto {
+	
+	private double descuento;
 
+	public ProductoPrimeraNecesidad(String nombre, double precio, double descuento) {
+		super(nombre, precio);
+		this.setDescuento(descuento);
+	}
+	
 	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esPrecioCuidado) {
 		super(nombre, precio, esPrecioCuidado);
 	}
+	
+	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esPrecioCiudado, double descuento) {
+		super(nombre, precio, esPrecioCiudado);
+		this.setDescuento(descuento);
+	}
 
 	public double getPrecio() {
-		return super.getPrecio() * 0.9;
+		return super.getPrecio() * this.getDescuento();
+	}
+
+	public double getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
 	}
 }
