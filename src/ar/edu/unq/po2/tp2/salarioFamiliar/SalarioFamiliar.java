c19265db4d12d20d34ddd62891bcd27302fa9fe2
Justeffie <florencia.aquinounq@gmail.com>
2021-09-20 20:14:33 -0300

    update uml

--- src/ar/edu/unq/po2/tp2/salarioFamiliar/SalarioFamiliar.java
+++ src/ar/edu/unq/po2/tp2/salarioFamiliar/SalarioFamiliar.java
@@ -2,21 +2,21 @@ package ar.edu.unq.po2.tp2.salarioFamiliar;
 
 public class SalarioFamiliar {
 	
-	private double cantidadAsignacionXHijo = 150.0;
+	private double montoFijoAsignacionXHijo = 150.0;
 	
-	private double cantidadSiTieneConyuge = 100.0;
+	private double montoFijoSiTieneConyuge = 100.0;
 	
-	private double montoPorAntiguedad = 50.0;
+	private double montoFijoPorAntiguedad = 50.0;
 
 	public double calcularMontoAsignacionPorHijo(int cantidadHijos) {
-		return this.cantidadAsignacionXHijo * cantidadHijos;
+		return this.montoFijoAsignacionXHijo * cantidadHijos;
 	}
 	
 	public double calcularMontoAsignacionPorConyuge(boolean tieneConyuge) {
-		return tieneConyuge ? this.cantidadSiTieneConyuge : 0l;
+		return tieneConyuge ? this.montoFijoSiTieneConyuge : 0l;
 	}
 	
 	public double calcularMontoPorAntiguedad(int antiguedad) {
-		return montoPorAntiguedad * antiguedad;
+		return montoFijoPorAntiguedad * antiguedad;
 	}
 }
