ed40b11ec940a7414fddeeb118cf7564451c06b4
Justeffie <florencia.aquinounq@gmail.com>
2021-09-20 17:56:47 -0300

    Update uml

--- src/ar/edu/unq/po2/tp2/sueldo/SueldoEmpleadoTemporario.java
+++ src/ar/edu/unq/po2/tp2/sueldo/SueldoEmpleadoTemporario.java
@@ -2,24 +2,24 @@ package ar.edu.unq.po2.tp2.sueldo;
 
 public class SueldoEmpleadoTemporario extends Sueldo {
 	
-	private double montoRetencionEdad = 25.0;
+	private double montoFijoRetencionEdad = 25.0;
 	
-	private double retencionAnios = 50.0;
+	private long edadDeRetencion = 50;
 	
-	private double retencionXHora = 5.0;
+	private double montoFijoRetencionXHora = 5.0;
 	
-	private double montoXHoraExtra = 40.0;
+	private double montoAdicionalXHoraExtra = 40.0;
 
 	public SueldoEmpleadoTemporario(double montoBasico) {
 		super(montoBasico);
 	}
 	
 	public double calcularMontoXHorasExtras(int horas) {
-		return this.montoXHoraExtra * horas;
+		return this.montoAdicionalXHoraExtra * horas;
 	}
 	
 	public double calcularRetencionXAnios(int edad) {
-		return edad > this.retencionAnios ? this.montoRetencionEdad : 0;
+		return edad > this.edadDeRetencion ? this.montoFijoRetencionEdad : 0;
 	}
 
 	@Override
@@ -28,6 +28,6 @@ public class SueldoEmpleadoTemporario extends Sueldo {
 	}
 	
 	public double retencionPorHoraExtra(int horas) {
-		return this.retencionXHora * horas;
+		return this.montoFijoRetencionXHora * horas;
 	}
 }
